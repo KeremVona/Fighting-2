c7ad7e1980fd52d08d2498b4e9396dbe4f6de92a
KeremVona <keremvona@gmail.com>
2025-11-18 20:28:16 +0300

    initial ui and frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,41 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  const [phase, setPhase] = useState(1);
+  const [planningPhaseValues, setPlanningPhaseValues] = useState({
+    altitude: 0,
+    speed: 0,
+    pilotSkill: 0,
+  });
+  return (
+    <>
+      <h1 className="text-2xl text-white font-bold p-2">Fighting 2</h1>
+      <div className="justify-center items-center flex bg-gray-600 min-h-screen">
+        <div className="border-white border-2 p-2 bg-gray-500 border-solid w-250 h-140">
+          <p className="">Phase {phase}</p>
+          <input
+            type="text"
+            placeholder="set altitude"
+            className="border-white border bg-black text-white mr-2 p-1"
+          />
+          <input
+            type="text"
+            placeholder="set speed"
+            className="border-white border bg-black text-white mr-2 p-1"
+          />
+          <input
+            type="text"
+            placeholder="set pilot skill"
+            className="border-white border bg-black text-white mr-2 p-1"
+          />
+          <button className="text-white bg-black border-white border p-1">
+            Go to the Battle
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
