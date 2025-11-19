25cf0f3d42e21ce584b72a0a115a1b2697c69b03
KeremVona <keremvona@gmail.com>
2025-11-19 19:26:12 +0300

    added more strategies and made some planning

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -28,3 +28,30 @@ function App() {
 }
 
 export default App;
+
+/*
+## phase 1
+
+DONE - phase 1 the values are set
+
+DONE - go to the next phase (phase 2)
+---------------------------------------
+## phase 2
+
+1. enemy strategy is revealed
+- get a strategy from strategies file
+- display on screen
+
+2. ask the user to choose a strategy
+- a grid is shown with the strategies from the file
+- when the card is clicked, the strategy is chosen
+
+3. the game is played for 5 seconds
+- show spinner
+
+4. the game stops and repeat from step 1 until the game is complete
+---------------------------------------
+## phase 2 advanced
+
+show enemy and friendly airplane count
+*/
