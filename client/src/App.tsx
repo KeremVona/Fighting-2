092b93b432212837e6e3cd5d2e2b66c93f14957a
KeremVona <keremvona@gmail.com>
2025-11-19 13:45:10 +0300

    work on the planning phase of the game

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,39 @@
 import { useState } from "react";
 import "./App.css";
 
+// TO DO
+// Display phases according to the one currently playing
+// Separate the components
+
 function App() {
   const [phase, setPhase] = useState(1);
   const [planningPhaseValues, setPlanningPhaseValues] = useState({
-    altitude: 0,
-    speed: 0,
-    pilotSkill: 0,
+    altitude: undefined,
+    speed: undefined,
+    pilotSkill: undefined,
   });
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setPlanningPhaseValues((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleClick = () => {
+    const { altitude, speed, pilotSkill } = planningPhaseValues;
+
+    if (
+      altitude === undefined ||
+      speed === undefined ||
+      pilotSkill === undefined
+    ) {
+      return; // TO DO: Display message
+    }
+
+    const check = altitude + speed;
+    const phasePValues = check + pilotSkill;
+
+    console.log(phasePValues);
+  };
   return (
     <>
       <h1 className="text-2xl text-white font-bold p-2">Fighting 2</h1>
@@ -16,20 +42,32 @@ function App() {
           <p className="">Phase {phase}</p>
           <input
             type="text"
-            placeholder="set altitude"
+            placeholder="set altitude (e.g. low, med, high)"
+            onChange={handleChange}
+            value={planningPhaseValues.altitude}
+            name="altitude"
             className="border-white border bg-black text-white mr-2 p-1"
           />
           <input
             type="text"
-            placeholder="set speed"
+            placeholder="set speed (e.g. low, med, high)"
+            onChange={handleChange}
+            value={planningPhaseValues.speed}
+            name="speed"
             className="border-white border bg-black text-white mr-2 p-1"
           />
           <input
             type="text"
-            placeholder="set pilot skill"
+            placeholder="set pilot skill (0-30)"
+            onChange={handleChange}
+            value={planningPhaseValues.pilotSkill}
+            name="pilotSkill"
             className="border-white border bg-black text-white mr-2 p-1"
           />
-          <button className="text-white bg-black border-white border p-1">
+          <button
+            onClick={() => console.log(planningPhaseValues.altitude)}
+            className="text-white bg-black border-white border p-1"
+          >
             Go to the Battle
           </button>
         </div>
