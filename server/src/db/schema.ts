c16db0edb93d177f0b53f2209509b477e021c1f1
KeremVona <keremvona@gmail.com>
2025-11-21 23:28:54 +0300

    get and the initial send

--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -1,1 +1,14 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import {
+  integer,
+  pgTable,
+  varchar,
+  primaryKey,
+  serial,
+} from "drizzle-orm/pg-core";
+
+export const strategies = pgTable("strategies", {
+  userId: serial().primaryKey(),
+  strategyName: varchar({ length: 255 }),
+  beats: varchar({ length: 255 }),
+  isBeatenBy: varchar({ length: 255 }),
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,38 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { strategies } from "./db/schema.js";
+import { is } from "drizzle-orm";
 
 const db = drizzle({
   connection: process.env.DATABASE_URL!,
-  casing: "snake_case",
 });
+
+interface StrategyRequestBody {
+  title: string;
+  beats: string;
+  isBeatenBy: string;
+}
+
+const fetchStrategies = async () => {
+  try {
+    const strategiesData = await db.select().from(strategies);
+
+    console.log("res: ", strategiesData);
+  } catch (error) {
+    console.error("Failed to fetch strategies:", error);
+  }
+};
+
+const sendStrategy = async (
+  req: Request<{}, {}, StrategyRequestBody>,
+  res: Response
+) => {
+  try {
+    const { title, beats, isBeatenBy } = req.body;
+    await db
+      .insert(strategies)
+      .values({ strategyName: title, beats: beats, isBeatenBy: isBeatenBy });
+  } catch (error) {
+    console.error("Failed to send strategy:", error);
+  }
+};
